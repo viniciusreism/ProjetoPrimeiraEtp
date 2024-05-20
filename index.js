16916954ee19a7461bf2b65e387a436a0efaeb73
Vinicius <viniciusmirandareis40@gmail.com>
2024-05-20 18:28:57 -0300

    PROJETO PRIM. ETAPA VINICIUS E MATEUS B. GAS E IMC

--- index.js
+++ index.js
@@ -2,16 +2,9 @@
  * @format
  */
 
-// import {AppRegistry} from 'react-native';
-// import App from './Appnew';
-// import {name as appName} from './app.json';
-// import Appnew from './Appnew';
-
-// AppRegistry.registerComponent(appName, () => App);
-
 import {AppRegistry} from 'react-native';
 import AppIMCR from './AppIMCr'
 import AppGas from './Appgas'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppIMCR);
+AppRegistry.registerComponent(appName, () => AppGas);
